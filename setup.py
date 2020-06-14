1cef084aa56580c803e3e88ca8d23fe88fbbbfab
Md Nazrul Islam <connect2nazrul@gmail.com>
2020-06-14 21:38:02 +0600

    :bug: fix extra definition in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["pydantic['email']"]
+requirements = ["pydantic[email]"]
 
 setup_requirements = ["pytest-runner"]
 

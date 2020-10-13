245f3bff1efcd74df527d0967e4be544a2e40d6f
Itay Goren <itay@alike.health>
2020-10-13 23:28:33 +0300

    add validate one of many to Order

--- fhir/resources/DSTU2/order.py
+++ fhir/resources/DSTU2/order.py
@@ -7,7 +7,7 @@ Revision: None
 """
 from typing import List as ListType
 
-from pydantic import Field 
+from pydantic import Field, root_validator
 
 from . import fhirtypes
 from .backboneelement import BackboneElement
@@ -128,6 +128,42 @@ class Order(DomainResource):
         # if property is element of this resource.
         element_property=True,
     )
+
+    @root_validator(pre=True)
+    def validate_one_of_many(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        """https://www.hl7.org/fhir/formats.html#choice
+        A few elements have a choice of more than one data type for their content.
+        All such elements have a name that takes the form nnn[x].
+        The "nnn" part of the name is constant, and the "[x]" is replaced with
+        the title-cased name of the type that is actually used.
+        The table view shows each of these names explicitly.
+        Elements that have a choice of data type cannot repeat - they must have a
+        maximum cardinality of 1. When constructing an instance of an element with a
+        choice of types, the authoring system must create a single element with a
+        data type chosen from among the list of permitted data types.
+        """
+        one_of_many_fields = {
+            "reason": ["reasonCodeableConcept", "reasonReference"],
+        }
+        for prefix, fields in one_of_many_fields.items():
+            assert cls.__fields__[fields[0]].field_info.extra["one_of_many"] == prefix
+            required = (
+                cls.__fields__[fields[0]].field_info.extra["one_of_many_required"]
+                is True
+            )
+            found = False
+            for field in fields:
+                if field in values and values[field] is not None:
+                    if found is True:
+                        raise ValueError(
+                            "Any of one field value is expected from "
+                            f"this list {fields}, but got multiple!"
+                        )
+                    else:
+                        found = True
+            if required is True and found is False:
+                raise ValueError(f"Expect any of field value from this list {fields}.")
+        return values
 
 
 

3f61a5e7963657ed354e2d02b938c62569583299
Md Nazrul Islam <connect2nazrul@gmail.com>
2023-12-14 13:28:36 +0100

    Back to development: 7.1.1

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ setup(
         "all": (orjson_requirements + yaml_requirements + xml_requirements),
     },
     url="https://github.com/nazrulworld/fhir.resources",
-    version="7.1.0",
+    version="7.1.1.dev0",
     zip_safe=False,
     python_requires=">=3.7",
     project_urls={
